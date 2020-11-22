35f3cb59681de2d2a4934f6459966d1eb3571dbb
Soumyanil Biswas <61424547+Soumyanil-Biswas@users.noreply.github>
2020-11-22 15:41:24 +0530

    Files Updated

--- Password_Gen/setup.sh
+++ Password_Gen/setup.sh
@@ -7,9 +7,29 @@ GREEN='\033[0;32m'
 echo -e "
 ${BLUE}Setting the script directory in system path${NC} ..."
 
-echo -e "
-${GREEN}Please provide your useraccount password to successfully complete the setup${NC}"
+echo $PATH > prev_SysPATH.txt
 
-sudo echo "export PATH="$PATH:/home/ubuntu/Desktop/thenewboston_bash/Hackersploit/Password_Gen"" >> ~/.bashrc
 
-sudo chmod 744 Password_Gen
+prsnt=`pwd`
+
+echo $prsnt
+
+#echo -e "${Blue}Which Shell do you use as default?${NC}: "
+#read shell
+
+#rc="rc"
+
+#echo $rc
+
+#file="$shell$rc"
+
+#echo "$file"
+#echo -e "
+#${GREEN}Please provide your useraccount password to successfully complete the setup${NC}"
+    
+#sudo echo "export PATH="$PATH:$prsnt"" >> ~/.file
+
+sudo echo "export PATH="$PATH:$prsnt"" >> ~/.bashrc
+
+sudo chmod 744 PasswordGenerator
+
--- README.md
+++ README.md
@@ -3,19 +3,19 @@
 ## For Installation Setup:
 &nbsp;
 ```
+$ cd Sh_PasswordGen/
 $ chmod 744 setup.sh
 $ ./setup.sh
 ```
 ## To run:
 &nbsp;
-#### Simply write:
+### Simply write:
 ```
 $ PasswordGenerator
 ```
 ## A Screenshot:
 
 ![alt text](https://github.com/Soumyanil-Biswas/Sh_PasswordGen/blob/main/Password_Gen/Screenshot.png?raw=true)
-
 ##### LinkedIn Profile
 [![LinkedIn](https://img.shields.io/badge/LinkedIn-brightgreen?style=flat-square&logo=linkedin&labelColor=blue)](https://www.linkedin.com/in/soumyanil-biswas-4b7603195)
 
